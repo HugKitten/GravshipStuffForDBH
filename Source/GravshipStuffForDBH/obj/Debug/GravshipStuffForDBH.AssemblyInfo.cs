409dbd61e84b7621aca5b3180c05cef3e21d901f
Kitty <>
2025-10-26 14:10:09 -0700

    Update mod name in def

--- Source/GravshipStuffForDBH/obj/Debug/GravshipStuffForDBH.AssemblyInfo.cs
+++ Source/GravshipStuffForDBH/obj/Debug/GravshipStuffForDBH.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GravshipStuffForDubsBadHygiene")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c9306b6b04ede432f0d06ebe7a61e66ba67491e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+02b4f164c0f6e0ce4be683b70b430682ad5d43e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("GravshipStuffForDubsBadHygiene")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GravshipStuffForDubsBadHygiene")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
